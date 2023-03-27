d1c621b1f01f9d92d2416be79beb08687baf6553
Ghislain Bourgeois <ghislain.bourgeois@gmail.com>
2023-03-27 14:47:03 -0400

    Add support for tls-certificates-interface V2

--- src/charm.py
+++ src/charm.py
@@ -7,22 +7,47 @@
 """Nginx static website charm."""
 
 import logging
+import pathlib
 
 from charms.observability_libs.v1.kubernetes_service_patch import KubernetesServicePatch
+from charms.tls_certificates_interface.v2.tls_certificates import (
+    TLSCertificatesRequiresV2,
+    generate_csr,
+    generate_private_key,
+)
 from lightkube.models.core_v1 import ServicePort
 from ops.charm import CharmBase
 from ops.main import main
-from ops.model import ActiveStatus
+from ops.model import ActiveStatus, BlockedStatus, MaintenanceStatus, WaitingStatus
 
 logger = logging.getLogger(__name__)
 
 
 class NginxOperatorCharm(CharmBase):
-    """Charm the service."""
+    """Charm nginx service."""
 
     def __init__(self, *args):
         super().__init__(*args)
+        self._container = self.unit.get_container("nginx")
+        self.certificates = TLSCertificatesRequiresV2(self, "certificates")
+
         self.framework.observe(self.on.nginx_pebble_ready, self._on_nginx_pebble_ready)
+        self.framework.observe(self.on.config_changed, self._request_certificate)
+        self.framework.observe(self.on.certificates_relation_joined, self._request_certificate)
+        self.framework.observe(
+            self.certificates.on.certificate_available, self._on_certificate_available
+        )
+        self.framework.observe(
+            self.certificates.on.certificate_expiring, self._request_certificate_renewal
+        )
+        self.framework.observe(
+            self.certificates.on.certificate_invalidated, self._request_certificate_renewal
+        )
+        self.framework.observe(
+            self.certificates.on.all_certificates_invalidated,
+            self._request_certificate_renewal,
+        )
+
         self.service_patcher = KubernetesServicePatch(
             charm=self,
             ports=[
@@ -34,11 +59,92 @@ class NginxOperatorCharm(CharmBase):
         )
 
     def _on_nginx_pebble_ready(self, event):
-        """Define and start a workload using the Pebble API."""
-        container = event.workload
-        container.add_layer("nginx", self._pebble_layer, combine=True)
-        container.replan()
+        """Define and start nginx service using the Pebble API."""
+        self.unit.status = MaintenanceStatus("Configuring nginx service")
+        self._container.add_layer("nginx", self._pebble_layer, combine=True)
+        self._container.replan()
+        self._container.restart("nginx")
         self.unit.status = ActiveStatus()
+
+    def _request_certificate(self, event):
+        """Request a certificate over the tls-certificates interface.
+
+        It will first generate a private key if not already present, create a CSR
+        and send that CSR over the relation.
+        """
+        if not self._container.can_connect():
+            self.unit.status = WaitingStatus("Waiting for nginx container to be ready")
+            event.defer()
+            return
+        replicas_relation = self.model.get_relation("replicas")
+        if not replicas_relation:
+            self.unit.status = WaitingStatus("Waiting for peer relation to be created")
+            event.defer()
+            return
+        if not self.model.get_relation("certificates"):
+            self.unit.status = WaitingStatus("Waiting for certificates relation to be created")
+            event.defer()
+            return
+        if not self._fqdn:
+            self.unit.status = BlockedStatus("FQDN configuration missing")
+            return
+        if "private_key" not in replicas_relation.data[self.app]:
+            self._generate_private_key()
+        private_key = replicas_relation.data[self.app].get("private_key")
+        csr = generate_csr(
+            private_key=private_key.encode(),
+            subject=self._fqdn,
+        )
+        replicas_relation.data[self.app].update({"csr": csr.decode()})
+        self.certificates.request_certificate_creation(certificate_signing_request=csr)
+        self.unit.status = WaitingStatus("Waiting for requested certificate")
+
+    def _request_certificate_renewal(self, event):
+        """Request a new certificate by providing a new CSR on the tls-certificates relation."""
+        if not self._fqdn:
+            self.unit.status = BlockedStatus("FQDN configuration missing")
+            return
+        replicas_relation = self.model.get_relation("replicas")
+        old_csr = replicas_relation.data[self.app].get("csr")
+        private_key = replicas_relation.data[self.app].get("private_key")
+        csr = generate_csr(
+            private_key=private_key.encode(),
+            subject=self._fqdn,
+        )
+        replicas_relation.data[self.app].update({"csr": csr.decode()})
+        self.certificates.request_certificate_renewal(
+            old_certificate_signing_request=old_csr.encode(), new_certificate_signing_request=csr
+        )
+        self.unit.status = WaitingStatus("Waiting for requested certificate")
+
+    def _on_certificate_available(self, event):
+        """Handle provided certificate.
+
+        When a certificate becomes available, push it to the container,
+        configure nginx for TLS, store the certificate in a peer relation and
+        force reconfiguration of nginx.
+        """
+        replicas_relation = self.model.get_relation("replicas")
+        if not replicas_relation:
+            self.unit.status = WaitingStatus("Waiting for peer relation to be created")
+            event.defer()
+            return
+        self._container.push(
+            path="/etc/nginx/ssl/cert.crt", source=event.certificate.encode(), make_dirs=True
+        )
+        self._push_tls_config()
+        replicas_relation.data[self.app].update({"certificate": event.certificate})
+        self._on_nginx_pebble_ready(event)
+
+    def _generate_private_key(self):
+        """Generate a private key.
+
+        Push it to the container and store it in the peer relation.
+        """
+        private_key = generate_private_key()
+        self._container.push(path="/etc/nginx/ssl/cert.key", source=private_key, make_dirs=True)
+        replicas_relation = self.model.get_relation("replicas")
+        replicas_relation.data[self.app].update({"private_key": private_key.decode()})
 
     @property
     def _pebble_layer(self):
@@ -55,6 +161,18 @@ class NginxOperatorCharm(CharmBase):
                 }
             },
         }
+
+    @property
+    def _fqdn(self):
+        """Property to get the FQDN from the charm configuration."""
+        return self.model.config.get("fqdn")
+
+    def _push_tls_config(self) -> None:
+        """Push tls server configuration to /etc/nginx/conf.d/tls.conf."""
+        with open(pathlib.Path(__file__).parent / "tls.conf") as config:
+            self._container.push(
+                path="/etc/nginx/conf.d/tls.conf", source=config.read(), make_dirs=True
+            )
 
 
 if __name__ == "__main__":  # pragma: nocover
--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -3,28 +3,41 @@
 #
 # Learn more about testing at: https://juju.is/docs/sdk/testing
 
-import unittest
+import pathlib
+import tempfile
 from unittest.mock import Mock, patch
 
-import ops.testing
+import pytest
 from charm import NginxOperatorCharm
-from ops.model import ActiveStatus
-from ops.testing import Harness
+from charms.tls_certificates_interface.v2.tls_certificates import (
+    generate_csr,
+    generate_private_key,
+)
+from ops.model import ActiveStatus, BlockedStatus, MaintenanceStatus, UnknownStatus, WaitingStatus
+from scenario.state import Container, Mount, Relation, State
 
 
-class TestCharm(unittest.TestCase):
+def _config_changed_event(_):
+    return "config_changed"
+
+
+def _relation_joined_event(x):
+    return x.joined_event
+
+
+class TestCharm:
     @patch("charm.KubernetesServicePatch", new=Mock)
-    def setUp(self):
-        ops.testing.SIMULATE_CAN_CONNECT = True
-        self.addCleanup(setattr, ops.testing, "SIMULATE_CAN_CONNECT", False)
+    def test_when_pebble_ready_then_nginx_is_started(self):
+        container = Container(name="nginx", can_connect=True)
+        out = State(
+            containers=[
+                container,
+            ]
+        ).trigger(container.pebble_ready_event, NginxOperatorCharm)
 
-        self.harness = Harness(NginxOperatorCharm)
-        self.addCleanup(self.harness.cleanup)
-        self.harness.begin()
-
-    def test_nginx_pebble_ready(self):
-        # Expected plan after Pebble ready with default config
-        expected_plan = {
+        expected_layer = {
+            "summary": "nginx pebble layer",
+            "description": "pebble config layer for nginx",
             "services": {
                 "nginx": {
                     "override": "replace",
@@ -34,9 +47,333 @@ class TestCharm(unittest.TestCase):
                 }
             },
         }
-        self.harness.container_pebble_ready("nginx")
-        updated_plan = self.harness.get_container_pebble_plan("nginx").to_dict()
-        self.assertEqual(expected_plan, updated_plan)
-        service = self.harness.model.unit.get_container("nginx").get_service("nginx")
-        self.assertTrue(service.is_running())
-        self.assertEqual(self.harness.model.unit.status, ActiveStatus())
+        assert out.containers[0].layers["nginx"].to_dict() == expected_layer
+        assert out.status.unit_history == [
+            UnknownStatus(),
+            MaintenanceStatus("Configuring nginx service"),
+        ]
+        assert out.status.unit == ActiveStatus()
+
+    @patch("charm.KubernetesServicePatch", new=Mock)
+    def test_cannot_connect_when_config_changed_then_event_is_defered(self):
+        container = Container(name="nginx", can_connect=False)
+        out = State(
+            containers=[
+                container,
+            ]
+        ).trigger("config_changed", NginxOperatorCharm)
+
+        assert out.deferred[0].name == "config_changed"
+        assert out.status.unit == WaitingStatus("Waiting for nginx container to be ready")
+
+    @patch("charm.KubernetesServicePatch", new=Mock)
+    def test_can_connect_and_no_peer_relation_when_config_changed_then_event_is_defered(self):
+        container = Container(name="nginx", can_connect=True)
+        out = State(
+            containers=[
+                container,
+            ]
+        ).trigger("config_changed", NginxOperatorCharm)
+
+        assert out.deferred[0].name == "config_changed"
+        assert out.status.unit == WaitingStatus("Waiting for peer relation to be created")
+
+    @patch("charm.KubernetesServicePatch", new=Mock)
+    def test_can_connect_and_peer_relation_and_no_certificates_relation_when_config_changed_then_event_is_defered(
+        self,
+    ):  # noqa E501
+        container = Container(name="nginx", can_connect=True)
+        peer_relation = Relation("replicas")
+        out = State(
+            containers=[
+                container,
+            ],
+            relations=[peer_relation],
+        ).trigger("config_changed", NginxOperatorCharm)
+
+        assert out.deferred[0].name == "config_changed"
+        assert out.status.unit == WaitingStatus("Waiting for certificates relation to be created")
+
+    @patch("charm.KubernetesServicePatch", new=Mock)
+    @pytest.mark.parametrize("event", [_config_changed_event, _relation_joined_event])
+    def test_can_connect_and_peer_relation_and_certificates_relation_and_no_fqdn_when_config_changed_then_status_is_blocked(
+        self, event
+    ):  # noqa E501
+        container = Container(name="nginx", can_connect=True)
+        peer_relation = Relation("replicas")
+        certificates_relation = Relation("certificates")
+        out = State(
+            containers=[
+                container,
+            ],
+            relations=[peer_relation, certificates_relation],
+        ).trigger(event(certificates_relation), NginxOperatorCharm)
+
+        assert len(out.deferred) == 0
+        assert out.status.unit == BlockedStatus("FQDN configuration missing")
+
+    @patch("charm.KubernetesServicePatch", new=Mock)
+    @pytest.mark.parametrize("event", [_config_changed_event, _relation_joined_event])
+    def test_no_private_key_when_config_changed_then_private_key_is_created(
+        self, event
+    ):  # noqa E501
+        local_file = tempfile.NamedTemporaryFile()
+        container = Container(
+            name="nginx",
+            can_connect=True,
+            mounts={"local": Mount("/etc/nginx/ssl/cert.key", local_file.name)},
+        )
+        peer_relation = Relation("replicas")
+        certificates_relation = Relation("certificates")
+        out = State(
+            leader=True,
+            containers=[
+                container,
+            ],
+            relations=[peer_relation, certificates_relation],
+            config={"fqdn": "test.fqdn"},
+        ).trigger(event(certificates_relation), NginxOperatorCharm)
+
+        assert len(out.deferred) == 0
+        assert len(local_file.read()) > 0
+        assert "private_key" in out.relations[0].local_app_data
+        assert out.status.unit == WaitingStatus("Waiting for requested certificate")
+
+    @patch("charm.KubernetesServicePatch", new=Mock)
+    @pytest.mark.parametrize("event", [_config_changed_event, _relation_joined_event])
+    def test_private_key_when_config_changed_then_private_key_is_not_recreated(
+        self, event
+    ):  # noqa E501
+        local_file = tempfile.NamedTemporaryFile()
+        container = Container(
+            name="nginx",
+            can_connect=True,
+            mounts={"local": Mount("/etc/nginx/ssl/cert.key", local_file.name)},
+        )
+        peer_relation = Relation("replicas")
+        peer_relation.local_app_data.update({"private_key": generate_private_key().decode()})
+        certificates_relation = Relation("certificates")
+        out = State(
+            leader=True,
+            containers=[
+                container,
+            ],
+            relations=[peer_relation, certificates_relation],
+            config={"fqdn": "test.fqdn"},
+        ).trigger(event(certificates_relation), NginxOperatorCharm)
+
+        assert len(out.deferred) == 0
+        assert (
+            out.relations[0].local_app_data["private_key"]
+            == peer_relation.local_app_data["private_key"]
+        )
+        assert out.status.unit == WaitingStatus("Waiting for requested certificate")
+
+    @patch("charm.KubernetesServicePatch", new=Mock)
+    @pytest.mark.parametrize("event", [_config_changed_event, _relation_joined_event])
+    def test_private_key_when_config_changed_then_csr_is_sent_in_relation_data(
+        self, event
+    ):  # noqa E501
+        container = Container(name="nginx", can_connect=True)
+        peer_relation = Relation("replicas")
+        peer_relation.local_app_data.update({"private_key": generate_private_key().decode()})
+        certificates_relation = Relation("certificates")
+        out = State(
+            leader=True,
+            containers=[
+                container,
+            ],
+            relations=[peer_relation, certificates_relation],
+            config={"fqdn": "test.fqdn"},
+        ).trigger(event(certificates_relation), NginxOperatorCharm)
+
+        assert len(out.deferred) == 0
+        assert (
+            "BEGIN CERTIFICATE REQUEST"
+            in out.relations[1].local_unit_data["certificate_signing_requests"]
+        )
+        assert out.status.unit == WaitingStatus("Waiting for requested certificate")
+
+    @patch("charm.KubernetesServicePatch", new=Mock)
+    def test_no_peer_relation_when_certificate_available_then_event_is_deferred(self):  # noqa E501
+        etc_nginx = tempfile.TemporaryDirectory()
+        container = Container(
+            name="nginx",
+            can_connect=True,
+            mounts={"local": Mount("/etc/nginx", etc_nginx.name)},
+        )
+        container = Container(name="nginx", can_connect=True)
+        private_key = generate_private_key().decode()
+        csr = generate_csr(
+            private_key=private_key.encode(),
+            subject="test.fqdn",
+        )
+        certificates_relation = Relation(
+            endpoint="certificates",
+            remote_app_name="remote",
+            local_unit_data={
+                "certificate_signing_requests": [{"certificate_signing_request": csr.decode()}]
+            },
+            remote_app_data={
+                "certificates": [
+                    {
+                        "ca": "test-ca",
+                        "chain": ["test-chain"],
+                        "certificate": "test-certificate",
+                        "certificate_signing_request": csr.decode(),
+                    }
+                ]
+            },
+        )
+        out = State(
+            leader=True,
+            containers=[
+                container,
+            ],
+            relations=[certificates_relation],
+            config={"fqdn": "test.fqdn"},
+        ).trigger(certificates_relation.changed_event, NginxOperatorCharm)
+
+        assert out.deferred[0].name == "certificate_available"
+        assert out.status.unit == WaitingStatus("Waiting for peer relation to be created")
+
+    @patch("charm.KubernetesServicePatch", new=Mock)
+    def test_when_certificate_available_then_tls_config_is_pushed(self):  # noqa E501
+        nginx_dir = tempfile.TemporaryDirectory()
+        container = Container(
+            name="nginx",
+            can_connect=True,
+            mounts={"nginx_dir": Mount("/etc/nginx", nginx_dir.name)},
+        )
+        private_key = generate_private_key().decode()
+        csr = generate_csr(
+            private_key=private_key.encode(),
+            subject="test.fqdn",
+        )
+        peer_relation = Relation(
+            endpoint="replicas", local_app_data={"private_key": private_key, "csr": csr.decode()}
+        )
+        certificates_relation = Relation(
+            endpoint="certificates",
+            remote_app_name="remote",
+            local_unit_data={
+                "certificate_signing_requests": [{"certificate_signing_request": csr.decode()}]
+            },
+            remote_app_data={
+                "certificates": [
+                    {
+                        "ca": "test-ca",
+                        "chain": ["test-chain"],
+                        "certificate": "test-certificate",
+                        "certificate_signing_request": csr.decode(),
+                    }
+                ]
+            },
+        )
+        out = State(
+            leader=True,
+            containers=[
+                container,
+            ],
+            relations=[peer_relation, certificates_relation],
+            config={"fqdn": "test.fqdn"},
+        ).trigger(certificates_relation.changed_event, NginxOperatorCharm)
+
+        assert out.status.unit == ActiveStatus()
+        with (
+            open(pathlib.Path(nginx_dir.name) / "conf.d" / "tls.conf") as tlsconf,
+            open(pathlib.Path(__file__).parent / "expected_tls.conf") as expected,
+        ):
+            assert tlsconf.read() == expected.read()
+        with open(pathlib.Path(nginx_dir.name) / "ssl" / "cert.crt") as cert:
+            assert cert.read() == "test-certificate"
+
+    @patch("charm.KubernetesServicePatch", new=Mock)
+    def test_has_certificate_when_certificate_invalidated_then_certificate_is_refreshed(
+        self,
+    ):  # noqa E501
+        container = Container(
+            name="nginx",
+            can_connect=True,
+        )
+        private_key = generate_private_key().decode()
+        csr = generate_csr(
+            private_key=private_key.encode(),
+            subject="test.fqdn",
+        )
+        peer_relation = Relation(
+            endpoint="replicas", local_app_data={"private_key": private_key, "csr": csr.decode()}
+        )
+        certificates_relation = Relation(
+            endpoint="certificates",
+            remote_app_name="remote",
+            local_unit_data={
+                "certificate_signing_requests": [{"certificate_signing_request": csr.decode()}]
+            },
+            remote_app_data={
+                "certificates": [
+                    {
+                        "ca": "test-ca",
+                        "chain": ["test-chain"],
+                        "certificate": "test-certificate",
+                        "certificate_signing_request": csr.decode(),
+                        "revoked": True,
+                    }
+                ]
+            },
+        )
+        out = State(
+            leader=True,
+            containers=[
+                container,
+            ],
+            relations=[peer_relation, certificates_relation],
+            config={"fqdn": "test.fqdn"},
+        ).trigger(certificates_relation.changed_event, NginxOperatorCharm)
+
+        assert out.status.unit == WaitingStatus("Waiting for requested certificate")
+
+    @patch("charm.KubernetesServicePatch", new=Mock)
+    def test_fqdn_config_removed_when_certificate_invalidated_then_status_is_blocked(
+        self,
+    ):  # noqa E501
+        container = Container(
+            name="nginx",
+            can_connect=True,
+        )
+        private_key = generate_private_key().decode()
+        csr = generate_csr(
+            private_key=private_key.encode(),
+            subject="test.fqdn",
+        )
+        peer_relation = Relation(
+            endpoint="replicas", local_app_data={"private_key": private_key, "csr": csr.decode()}
+        )
+        certificates_relation = Relation(
+            endpoint="certificates",
+            remote_app_name="remote",
+            local_unit_data={
+                "certificate_signing_requests": [{"certificate_signing_request": csr.decode()}]
+            },
+            remote_app_data={
+                "certificates": [
+                    {
+                        "ca": "test-ca",
+                        "chain": ["test-chain"],
+                        "certificate": "test-certificate",
+                        "certificate_signing_request": csr.decode(),
+                        "revoked": True,
+                    }
+                ]
+            },
+        )
+        out = State(
+            leader=True,
+            containers=[
+                container,
+            ],
+            relations=[peer_relation, certificates_relation],
+            config={"fqdn": ""},
+        ).trigger(certificates_relation.changed_event, NginxOperatorCharm)
+
+        assert out.status.unit == BlockedStatus("FQDN configuration missing")
